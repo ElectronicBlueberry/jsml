0029e1655f43088eb64a742849de83f3937c1e0f
ElectronicBlueberry <mail@lailalos.com>
2019-07-28 18:12:34 +0200

    set basic structure

---
+++ source/jsml.ts
@@ -0,0 +1,21 @@
+import * as config from './config.json';
+import * as help from './misc/help';
+
+const args: arguments = require('minimist')(process.argv.slice(2));
+
+if (args.help) {
+	console.log(help.print(args));
+	process.exit(0);
+}
+
+if (args.transpile) {
+	// Transpile the .jsml file into javascript
+}
+
+if (args.build) {
+	// Run the build step, running the transpiled javascript
+}
+
+if (!args.build && !args.transpile) {
+	// Default behaviour, Transpile and Build
+}
