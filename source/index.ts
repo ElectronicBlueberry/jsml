41d8e5e3218243fe398d62eebdb96b2377fbab6b
ElectronicBlueberry <mail@lailalos.com>
2019-07-30 13:34:56 +0200

    renaming and restucturing

--- source/index.ts
+++ source/index.ts
@@ -1,46 +1,30 @@
-import * as cnfg from './config.json';
-
-let currentConfig: jsml.config;
-resetConfig();
+import { PathLike } from "fs";
 
 /**
  * Builds .jsml templates into .html
  * @param input_folder Path to .jsml template input folder
  * @param output_folder Path to .html output folder
  * @param content_folder Path to content folder, which will be fed to the .jsml templates
- * @param options options for the .jsml templates
+ * @param options compiler options
+ * @param parameters parameters to pass to template
  */
-function build(input_folder: string, output_folder: string, content_folder: string, options: jsml.options) {}
+function build(input_folder: PathLike, output_folder: PathLike, content_folder: PathLike, options?: jsml.options, parameters?: jsml.parameters) {}
 
 /**
- * Transpiles .jsml files to .js files
+ * Compiles .jsml files to .js template files
  * @param input_folder .jsml input folder
- * @param output_folder .js output folder
+ * @param output_folder .js template output folder
+ * @param options compiler options
  */
-function transpile(input_folder: string, output_folder: string) {}
+function compile(input_folder: PathLike, output_folder: PathLike, options?: jsml.options) {}
 
 /**
  * Runs .js template files, which have been transpiled from .jsml, to generate .html files
  * @param input_folder Path to .js template input folder
  * @param output_folder Path to .html output folder
  * @param content_folder Path to content folder, which will be fed to the .jsml templates
- * @param options options for the .jsml templates
+ * @param parameters parameters to pass to template
  */
-function run(input_folder: string, output_folder: string, content_folder: string, options: jsml.options) {}
+function template(input_folder: PathLike, output_folder: PathLike, content_folder: PathLike, parameters?: jsml.parameters) {}
 
-/**
- * Overwrite the current configuration
- * @param config config object
- */
-function setConfig(config: jsml.config) {
-	Object.assign(currentConfig, config);
-}
-
-/**
- * Resets Configuration to default
- */
-function resetConfig() {
-	currentConfig = JSON.parse(JSON.stringify(cnfg));
-}
-
-export { build, transpile, run, setConfig, resetConfig };
+export { build, compile, template};
--- source/types/jsml.d.ts
+++ source/types/jsml.d.ts
@@ -1,9 +1,24 @@
 declare namespace jsml {
-	type arguments = { build: string; help: boolean; file: string };
-	type config = {};
+	type arguments = { build: string; help: boolean; file: import("fs").PathLike };
+	
 	/**
-     * The key must correspond to the template name
-     * The Value is then passed to the template
+     * Paramters to pass to the template
      */
-	type options = { [key: string]: any };
+	interface parameters {
+		/**
+		 * The key must correspond to the template name
+		 * The Value is then passed to the template
+		 */
+		[key: string]: any
+	}
+
+	/**
+	 * Compiler Options
+	 */
+	interface options {
+		/**
+		 * Path, where to chache .js templates
+		 */
+		cache: import("fs").PathLike
+	}
 }
